be765f487a9af4e73dc9d3fb03733454a8d4433c
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-24 17:48:30 +0100

    ADD Person class ADD Inheritance for Student and Professor

--- _src/OOP_2020-03-23_3-Universityy/it/polito/oop/university/Person.java
+++ _src/OOP_2020-03-23_3-Universityy/it/polito/oop/university/Person.java
@@ -11,8 +11,24 @@
 
 package it.polito.oop.university;
 
-public class Student extends Person {
-	Student(String id, String name) {
-		super(id, name);
+public class Person {
+	private String id;
+    private String name;
+	
+	Person(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	void setName(String name) {
+		this.name = name;
 	}
 }
